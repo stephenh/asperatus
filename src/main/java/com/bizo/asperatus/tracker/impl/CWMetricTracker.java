ba1fd990f4c2fed34db3c3a1ea1a4c6609e8051d
Stephen Haberman <stephen.haberman@gmail.com>
2015-04-20 17:09:29 -0500

    Update CWMetricTracker.close to flush the queue, have test use that.

--- src/main/java/com/bizo/asperatus/tracker/impl/CWMetricTracker.java
+++ src/main/java/com/bizo/asperatus/tracker/impl/CWMetricTracker.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
@@ -24,6 +24,10 @@ import com.bizo.asperatus.tracker.impl.buffer.MetricBuffer;
  * @author larry
  */
 public final class CWMetricTracker extends AbstractMetricTracker implements MetricTracker {
+
+  private static final Logger log = Logger.getLogger(CWMetricTracker.class.getName());
+  private final AmazonCloudWatch cloudwatch;
+  private final String namespace;
   private final MetricBuffer buffer = new InMemoryTracker();
   private final ScheduledExecutorService executor;
 
@@ -32,53 +36,54 @@ public final class CWMetricTracker extends AbstractMetricTracker implements Metr
       final String namespace,
       final ScheduledExecutorService executor,
       final long flushDelay,
-      final TimeUnit flushDelayUnit
-      ) {
+      final TimeUnit flushDelayUnit) {
+    this.cloudwatch = cloudwatch;
+    this.namespace = namespace;
     this.executor = executor;
-    executor.scheduleAtFixedRate(
-      new PushStatsTask(cloudwatch, namespace, buffer, executor),
-      flushDelay,
-      flushDelay,
-      flushDelayUnit);
+    executor.scheduleAtFixedRate(new PushStatsTask(executor), flushDelay, flushDelay, flushDelayUnit);
   }
 
   @Override
-  public void track(final String metricName, final Number value, final Unit unit, final Collection<CompoundDimension> dimensions) {
+  public void track(
+      final String metricName,
+      final Number value,
+      final Unit unit,
+      final Collection<CompoundDimension> dimensions) {
     buffer.track(metricName, value, unit, dimensions);
   }
 
   @Override
   public void close() {
     try {
-      // TODO: this should really just pull out any pending tasks and execute them....
+      Future<?> f = executor.submit(new PushStatsTask(executor));
+      f.get(5, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException e) {
+      log.log(Level.SEVERE, "Exception closing asperatus", e);
+    } catch (TimeoutException e) {
+      log.log(Level.SEVERE, "Exception closing asperatus", e);
+    }
+
+    try {
+      executor.shutdown();
       executor.awaitTermination(5, TimeUnit.SECONDS);
     } catch (final InterruptedException ie) {
       Thread.currentThread().interrupt();
-      // fall through
     }
-    executor.shutdownNow();
   }
 
-  private static final class PushStatsTask implements Runnable {
+  private final class PushStatsTask implements Runnable {
     private static final int MAX_PER_REQUEST = 10;
     private static final int NUM_RETRIES = 5;
     private static final int RETRY_DELAY_SEC = 30;
-    private final AmazonCloudWatch cloudwatch;
-    private final MetricBuffer buffer;
-    private final String namespace;
     private final RetryingScheduler scheduler;
 
-    private PushStatsTask(
-        final AmazonCloudWatch cloudwatch,
-        final String namespace,
-        final MetricBuffer buffer,
-        final ScheduledExecutorService executor) {
-      this.cloudwatch = cloudwatch;
-      this.namespace = namespace;
-      this.buffer = buffer;
+    private PushStatsTask(final ScheduledExecutorService executor) {
       scheduler = new RetryingScheduler(executor);
     }
 
+    @Override
     public void run() {
       final Map<MetricKey, MetricStatistics> stats = buffer.reset();
       if (stats.isEmpty()) {
@@ -90,9 +95,7 @@ public final class CWMetricTracker extends AbstractMetricTracker implements Metr
       // batch up cloudwatch requests
       for (final Map.Entry<MetricKey, MetricStatistics> me : stats.entrySet()) {
         final Aggregation a = toAggregation(me.getKey(), me.getValue());
-
         agg.add(a);
-
         if (agg.size() >= MAX_PER_REQUEST) {
           submit(agg);
           agg.clear();
@@ -104,42 +107,36 @@ public final class CWMetricTracker extends AbstractMetricTracker implements Metr
 
     private void submit(final List<Aggregation> agg) {
       if (agg.size() > 0) {
-        final CloudwatchSubmitTask task =
-          new CloudwatchSubmitTask(cloudwatch, namespace, new ArrayList<Aggregation>(agg));
+        final CloudwatchSubmitTask task = new CloudwatchSubmitTask(new ArrayList<Aggregation>(agg));
         scheduler.schedule(task, NUM_RETRIES, RETRY_DELAY_SEC);
       }
     }
-
-    private static final Aggregation toAggregation(final MetricKey key, final MetricStatistics val) {
-      return new Aggregation(
-        key.getMetricName(),
-        val.getSamples(),
-        val.getSum(),
-        val.getMin(),
-        val.getMax(),
-        val.getUnit(),
-        key.getDimension().getDimensions());
-    }
   }
 
-  private static final class CloudwatchSubmitTask implements Callable<Boolean> {
-    private final AmazonCloudWatch cloudwatch;
-    private final String namespace;
+  private static final Aggregation toAggregation(final MetricKey key, final MetricStatistics val) {
+    return new Aggregation(
+      key.getMetricName(),
+      val.getSamples(),
+      val.getSum(),
+      val.getMin(),
+      val.getMax(),
+      val.getUnit(),
+      key.getDimension().getDimensions());
+  }
+
+  private final class CloudwatchSubmitTask implements Callable<Boolean> {
     private final List<Aggregation> agg;
 
-    public CloudwatchSubmitTask(final AmazonCloudWatch cloudwatch, final String namespace, final List<Aggregation> agg) {
-      this.cloudwatch = cloudwatch;
-      this.namespace = namespace;
+    public CloudwatchSubmitTask(final List<Aggregation> agg) {
       this.agg = agg;
     }
 
+    @Override
     public Boolean call() throws Exception {
       final PutMetricDataRequest request = new PutMetricDataRequest();
       request.setNamespace(namespace);
       request.setMetricData(AggregationUtils.toMetricDatum(agg));
-      
       cloudwatch.putMetricData(request);
-
       return Boolean.TRUE;
     }
 
